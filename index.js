71145bc8c843b5091c0d503230c09ebbb9504bb5
Your Name <you@example.com>
2024-04-12 09:56:10 +0700

    commit from wr

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const morgan = require("morgan");
 const verifyToken = require('./services/verify-token');
+const path = require('path');
 
 const app = express();
 dotenv.config();
@@ -13,11 +14,14 @@ app.use(morgan("dev"));
 app.use(bodyParse.urlencoded({ extended: false }));
 app.use(bodyParse.json());
 
+const staticFilesDirectory = path.join(__dirname, 'public');
+app.use(express.static(staticFilesDirectory));
+
 app.use('/api/auth', require('./api/auth'));
 // app.use('/api/users/account', require('./api/users/account'));
 
 // Middleware xác thực chỉ áp dụng cho các endpoint cần được bảo vệ
-app.use(['/', '/api/users', '/api/danhmucs' ], verifyToken);
+app.use(['/', '/api', '/api/users', '/api/danhmucs' ], verifyToken);
 
 app.get('/', (req, res) => {
     res.send('<h1>🤖 API SQLSERVER from NODEJS - TEST</h1>');
