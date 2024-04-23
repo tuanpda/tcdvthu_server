8dca52d8a93994b87c7afebeb9641fa72e55468a
Your Name <you@example.com>
2024-04-24 05:22:07 +0700

    commit

--- api/kekhai.js
+++ api/kekhai.js
@@ -113,44 +113,44 @@ router.post("/kekhai-trans", async (req, res) => {
 
   table.columns.add("tylengansachdiaphuong", Int, { nullable: true });
   table.columns.add("hotrokhac", Int, { nullable: true });
-  table.columns.add("tungay", Date, { nullable: true });
+  // table.columns.add("tungay", Date, { nullable: true });
   table.columns.add("tyledong", Int, { nullable: true });
   table.columns.add("muctiendong", Float, { nullable: true });
   table.columns.add("maphuongthucdong", NVarChar, { nullable: true });
   table.columns.add("tenphuongthucdong", NVarChar, { nullable: true });
 
-  table.columns.add("sothang", Int, { nullable: true });
-  table.columns.add("tuthang", Date, { nullable: true });
-  table.columns.add("tientunguyendong", Float, { nullable: true });
-  table.columns.add("tienlai", Float, { nullable: true });
-  table.columns.add("madoituong", NVarChar, { nullable: true });
-  table.columns.add("tendoituong", NVarChar, { nullable: true });
-  table.columns.add("tylensnnht", Int, { nullable: true });
-  table.columns.add("tiennsnnht", Float, { nullable: true });
-  table.columns.add("tylensdp", Int, { nullable: true });
-  table.columns.add("tiennsdp", NVarChar, { nullable: true });
+  // table.columns.add("sothang", Int, { nullable: true });
+  // table.columns.add("tuthang", Date, { nullable: true });
+  // table.columns.add("tientunguyendong", Float, { nullable: true });
+  // table.columns.add("tienlai", Float, { nullable: true });
+  // table.columns.add("madoituong", NVarChar, { nullable: true });
+  // table.columns.add("tendoituong", NVarChar, { nullable: true });
+  // table.columns.add("tylensnnht", Int, { nullable: true });
+  // table.columns.add("tiennsnnht", Float, { nullable: true });
+  // table.columns.add("tylensdp", Int, { nullable: true });
+  // table.columns.add("tiennsdp", NVarChar, { nullable: true });
 
-  table.columns.add("matinh", NVarChar, { nullable: true });
-  table.columns.add("tentinh", NVarChar, { nullable: true });
-  table.columns.add("maquanhuyen", NVarChar, { nullable: true });
-  table.columns.add("tenquanhuyen", NVarChar, { nullable: true });
-  table.columns.add("maxaphuong", NVarChar, { nullable: true });
-  table.columns.add("tenxaphuong", NVarChar, { nullable: true });
-  table.columns.add("tothon", NVarChar, { nullable: true });
-  table.columns.add("benhvientinh", NVarChar, { nullable: true });
-  table.columns.add("mabenhvien", NVarChar, { nullable: true });
-  table.columns.add("tenbenhvien", NVarChar, { nullable: true });
-  table.columns.add("ghichu", NVarChar, { nullable: true });
+  // table.columns.add("matinh", NVarChar, { nullable: true });
+  // table.columns.add("tentinh", NVarChar, { nullable: true });
+  // table.columns.add("maquanhuyen", NVarChar, { nullable: true });
+  // table.columns.add("tenquanhuyen", NVarChar, { nullable: true });
+  // table.columns.add("maxaphuong", NVarChar, { nullable: true });
+  // table.columns.add("tenxaphuong", NVarChar, { nullable: true });
+  // table.columns.add("tothon", NVarChar, { nullable: true });
+  // table.columns.add("benhvientinh", NVarChar, { nullable: true });
+  // table.columns.add("mabenhvien", NVarChar, { nullable: true });
+  // table.columns.add("tenbenhvien", NVarChar, { nullable: true });
+  // table.columns.add("ghichu", NVarChar, { nullable: true });
 
-  table.columns.add("createdAt", NVarChar, { nullable: true });
-  table.columns.add("createdBy", NVarChar, { nullable: true });
-  table.columns.add("updatedAt", NVarChar, { nullable: true });
-  table.columns.add("updatedBy", NVarChar, { nullable: true });
+  // table.columns.add("createdAt", NVarChar, { nullable: true });
+  // table.columns.add("createdBy", NVarChar, { nullable: true });
+  // table.columns.add("updatedAt", NVarChar, { nullable: true });
+  // table.columns.add("updatedBy", NVarChar, { nullable: true });
 
-  table.columns.add("dotkekhai", NVarChar, { nullable: true });
-  table.columns.add("kykekhai", NVarChar, { nullable: true });
-  table.columns.add("ngaykekhai", DateTime, { nullable: true });
-  table.columns.add("trangthai", Bit, { nullable: true });
+  // table.columns.add("dotkekhai", NVarChar, { nullable: true });
+  // table.columns.add("kykekhai", NVarChar, { nullable: true });
+  // table.columns.add("ngaykekhai", DateTime, { nullable: true });
+  // table.columns.add("trangthai", Bit, { nullable: true });
 
   // Tạo số hồ sơ duy nhất
   const maxSoHoSoResult = await pool.request().query(
