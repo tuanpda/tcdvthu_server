eb01badb8be32320b0912d864895d0530d558814
Your Name <you@example.com>
2024-04-24 20:57:33 +0700

    commit

--- api/auth.js
+++ api/auth.js
@@ -67,61 +67,76 @@ router.post("/access/login", async (req, res, next) => {
   }
 });
 
-/* Tạo user auth kèm token */
-router.post("/account", upload.single("avatar"), async (req, res) => {
+router.post("/callresetpass", upload.single("avatar"), async (req, res) => {
   // console.log(req.body);
-  var password = req.body.password;
-  const encryptedPassword = await bcrypt.hash(password, 10);
-
-  let linkAvatar;
-  const file = req.file;
-  if (!file) {
-    linkAvatar = req.body.avatar;
-  } else {
-    // Đổi đường dẫn khi deploy lên máy chủ
-    linkAvatar = `http://ansinhbhxh.online/avatar/${req.file.filename}`;
-  }
   try {
     await pool.connect();
     const result = await pool
       .request()
-      .input("matochuc", req.body.matochuc)
-      .input("tentochuc", req.body.tentochuc)
-      .input("matinh", req.body.matinh)
-      .input("tentinh", req.body.tentinh)
-      .input("mahuyen", req.body.mahuyen)
-      .input("tenhuyen", req.body.tenhuyen)
-      .input("maxa", req.body.maxa)
-      .input("tenxa", req.body.tenxa)
-      .input("madaily", req.body.madaily)
-      .input("tendaily", req.body.tendaily)
-      .input("diachi", req.body.diachi)
-      .input("cccd", req.body.cccd)
-      .input("sodienthoai", req.body.sodienthoai)
       .input("email", req.body.email)
-      .input("username", req.body.username)
-      .input("name", req.body.name)
-      .input("password", encryptedPassword)
-      .input("role", req.body.role)
-      .input("avatar", linkAvatar)
-      .input("active", req.body.active)
-      .input("createdBy", req.body.createdBy)
-      .input("createdAt", req.body.createdAt)
-      .input("updatedBy", req.body.updatedBy)
-      .input("updatedAt", req.body.updatedAt).query(`
-                INSERT INTO users (matochuc, tentochuc, matinh, tentinh, mahuyen, tenhuyen, maxa, tenxa, madaily, tendaily,
-                  diachi, cccd, sodienthoai, email, username, name, password, role, avatar, active, 
-                  createdBy, createdAt, updatedBy, updatedAt) 
-                VALUES (@matochuc, @tentochuc, @matinh, @tentinh, @mahuyen, @tenhuyen, @maxa, @tenxa, @madaily, @tendaily,
-                  @diachi, @cccd, @sodienthoai, @email, @username, @name, @password, @role, @avatar, @active,
-                  @createdBy, @createdAt, @updatedBy, @updatedAt);
-            `);
-    const user = req.body;
-    let token = jwt.sign({ user }, process.env.SECRET, { expiresIn: "12h" });
-    res.json({ user, token, message: "Create user success!", success: true });
+      .query(`SELECT email, cccd, masobhxh FROM users where active=1 and email=@email`);
+    const infoAcc = result.recordset;
+    res.json(infoAcc);
   } catch (error) {
     res.status(500).json(error);
   }
 });
+
+/* Tạo user auth kèm token */
+// router.post("/account", upload.single("avatar"), async (req, res) => {
+//   // console.log(req.body);
+//   var password = req.body.password;
+//   const encryptedPassword = await bcrypt.hash(password, 10);
+
+//   let linkAvatar;
+//   const file = req.file;
+//   if (!file) {
+//     linkAvatar = req.body.avatar;
+//   } else {
+//     // Đổi đường dẫn khi deploy lên máy chủ
+//     linkAvatar = `http://ansinhbhxh.online/avatar/${req.file.filename}`;
+//   }
+//   try {
+//     await pool.connect();
+//     const result = await pool
+//       .request()
+//       .input("matochuc", req.body.matochuc)
+//       .input("tentochuc", req.body.tentochuc)
+//       .input("matinh", req.body.matinh)
+//       .input("tentinh", req.body.tentinh)
+//       .input("mahuyen", req.body.mahuyen)
+//       .input("tenhuyen", req.body.tenhuyen)
+//       .input("maxa", req.body.maxa)
+//       .input("tenxa", req.body.tenxa)
+//       .input("madaily", req.body.madaily)
+//       .input("tendaily", req.body.tendaily)
+//       .input("diachi", req.body.diachi)
+//       .input("cccd", req.body.cccd)
+//       .input("sodienthoai", req.body.sodienthoai)
+//       .input("email", req.body.email)
+//       .input("username", req.body.username)
+//       .input("name", req.body.name)
+//       .input("password", encryptedPassword)
+//       .input("role", req.body.role)
+//       .input("avatar", linkAvatar)
+//       .input("active", req.body.active)
+//       .input("createdBy", req.body.createdBy)
+//       .input("createdAt", req.body.createdAt)
+//       .input("updatedBy", req.body.updatedBy)
+//       .input("updatedAt", req.body.updatedAt).query(`
+//                 INSERT INTO users (matochuc, tentochuc, matinh, tentinh, mahuyen, tenhuyen, maxa, tenxa, madaily, tendaily,
+//                   diachi, cccd, sodienthoai, email, username, name, password, role, avatar, active,
+//                   createdBy, createdAt, updatedBy, updatedAt)
+//                 VALUES (@matochuc, @tentochuc, @matinh, @tentinh, @mahuyen, @tenhuyen, @maxa, @tenxa, @madaily, @tendaily,
+//                   @diachi, @cccd, @sodienthoai, @email, @username, @name, @password, @role, @avatar, @active,
+//                   @createdBy, @createdAt, @updatedBy, @updatedAt);
+//             `);
+//     const user = req.body;
+//     let token = jwt.sign({ user }, process.env.SECRET, { expiresIn: "12h" });
+//     res.json({ user, token, message: "Create user success!", success: true });
+//   } catch (error) {
+//     res.status(500).json(error);
+//   }
+// });
 
 module.exports = router;
